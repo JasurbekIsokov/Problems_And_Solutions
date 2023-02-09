267b87600d79add06551b41e19039621b7363c8b
Jasurbek <jasurisoqov685@gmail.com>
2023-02-09 23:17:57 +0500

    Shart Operatorlariga oid masalalar 1-10

---
+++ C++_Qudrat_Abdurahimov/Shart_Operatorlariga_Oid_Masalalar/Shart_Operatoriga_Oid_Masalalar/script.js
@@ -0,0 +1,209 @@
+"use script";
+
+// // Issue 1:
+
+// let number = Number(prompt("Enter a Number"));
+
+// if (number > 0) {
+//   number += 1;
+// }
+
+// console.log(number);
+
+// // --------------------------------------------------
+
+// // Issue 2:
+
+// let number = Number(prompt("Enter a Number"));
+
+// if (number > 0) {
+//   number += 1;
+// } else if (number < 0) {
+//   number -= 2;
+// }
+
+// console.log(number);
+
+// --------------------------------------------------
+
+// Issue 3:
+
+// let number = Number(prompt("Enter a Number"));
+
+// if (number > 0) {
+//   number += 1;
+// } else if (number < 0) {
+//   number -= 2;
+// } else {
+//   number = 10;
+// }
+
+// console.log(number);
+
+// --------------------------------------------------
+
+// Issue 4:
+
+// let number1 = Number(prompt("Enter a Number A A!=0"));
+// let number2 = Number(prompt("Enter a Number B B!=0"));
+// let number3 = Number(prompt("Enter a Number C C!=0"));
+
+// let musbatSum;
+
+// if (number1 > 0 && number2 > 0 && number3 > 0) {
+//   musbatSum = 3;
+// } else if (number1 > 0 && number2 > 0 && number3 < 0) {
+//   musbatSum = 2;
+// } else if (number1 > 0 && number2 < 0 && number3 > 0) {
+//   musbatSum = 2;
+// } else if (number1 < 0 && number2 > 0 && number3 > 0) {
+//   musbatSum = 2;
+// } else if (number1 > 0 && number2 < 0 && number3 < 0) {
+//   musbatSum = 1;
+// } else if (number1 < 0 && number2 > 0 && number3 < 0) {
+//   musbatSum = 1;
+// } else if (number1 < 0 && number2 < 0 && number3 > 0) {
+//   musbatSum = 1;
+// } else {
+//   musbatSum = 0;
+// }
+
+// console.log(musbatSum);
+
+// --------------------------------------------------
+
+// Issue 5:
+
+// let number1 = Number(prompt("Enter a Number A A!=0"));
+// let number2 = Number(prompt("Enter a Number B B!=0"));
+// let number3 = Number(prompt("Enter a Number C C!=0"));
+
+// let musbatSum;
+// let manfiySum;
+
+// if (number1 > 0 && number2 > 0 && number3 > 0) {
+//   musbatSum = 3;
+//   manfiySum = 0;
+// } else if (number1 > 0 && number2 > 0 && number3 < 0) {
+//   musbatSum = 2;
+//   manfiySum = 1;
+// } else if (number1 > 0 && number2 < 0 && number3 > 0) {
+//   musbatSum = 2;
+//   manfiySum = 1;
+// } else if (number1 < 0 && number2 > 0 && number3 > 0) {
+//   musbatSum = 2;
+//   manfiySum = 1;
+// } else if (number1 > 0 && number2 < 0 && number3 < 0) {
+//   musbatSum = 1;
+//   manfiySum = 2;
+// } else if (number1 < 0 && number2 > 0 && number3 < 0) {
+//   musbatSum = 1;
+//   manfiySum = 2;
+// } else if (number1 < 0 && number2 < 0 && number3 > 0) {
+//   musbatSum = 1;
+//   manfiySum = 2;
+// } else {
+//   musbatSum = 0;
+// }
+
+// console.log(`Musbat Number: ${musbatSum}, Manfiy Number: ${manfiySum}`);
+
+// --------------------------------------------------
+
+// Issue 6:
+
+// let number1 = Number(prompt("Enter a Number A"));
+// let number2 = Number(prompt("Enter a Number B"));
+
+// let maxNum;
+
+// if (number1 > number2) {
+//   maxNum = `Max Number: ${number1}`;
+// } else if (number1 === number2) {
+//   maxNum = "A = B";
+// } else {
+//   maxNum = `Max Number: ${number2}`;
+// }
+
+// console.log(maxNum);
+
+// // --------------------------------------------------
+
+// // Issue 7:
+
+// let number1 = Number(prompt("Enter a Number A"));
+// let number2 = Number(prompt("Enter a Number B"));
+
+// let maxNum;
+
+// if (number1 > number2) {
+//   maxNum = `Min Number: ${number2} || Sort Number 2`;
+// } else if (number1 === number2) {
+//   maxNum = "A = B";
+// } else {
+//   maxNum = `Min Number: ${number1} || Sort Number 1`;
+// }
+
+// console.log(maxNum);
+
+// // --------------------------------------------------
+
+// // Issue 8:
+
+// let number1 = Number(prompt("Enter a Number A"));
+// let number2 = Number(prompt("Enter a Number B"));
+
+// let maxNum;
+
+// if (number1 > number2) {
+//   maxNum = `Max Number: ${number1}, Min Number: ${number2}`;
+// } else if (number1 === number2) {
+//   maxNum = "A = B";
+// } else {
+//   maxNum = `Max Number: ${number2}, Min Number: ${number1}`;
+// }
+
+// console.log(maxNum);
+
+// --------------------------------------------------
+
+// Issue 9:
+
+// let a = Number(prompt("Enter a Number A"));
+// let b = Number(prompt("Enter a Number B"));
+
+// let maxNum;
+// let minNum;
+
+// if (a === b) {
+//   maxNum = "A = B";
+// } else if (a > b) {
+//   maxNum = a;
+//   minNum = b;
+// } else {
+//   maxNum = a;
+//   minNum = b;
+// }
+
+// console.log(`a = ${(a = minNum)}; b = ${(b = maxNum)}`);
+// console.log(a, b);
+
+// // --------------------------------------------------
+
+// // Issue 10:
+
+// let a = Number(prompt("Enter a Number A"));
+// let b = Number(prompt("Enter a Number B"));
+
+// let sumNum;
+
+// if (a !== b) {
+//   sumNum = a + b;
+// } else {
+//   sumNum = 0;
+// }
+
+// console.log(`a = ${(a = sumNum)}; b = ${(b = sumNum)}`);
+// console.log(a, b);
+
+// --------------------------------------------------
